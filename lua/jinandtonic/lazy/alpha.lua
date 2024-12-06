12b0ff6c3624a70ec5b872dbc34cc91df47407e6
周金 <steve.zhou@dbappsecurity.com.cn>
2024-12-06 06:40:56 -0800

    add alpha

---
+++ lua/jinandtonic/lazy/alpha.lua
@@ -0,0 +1,22 @@
+return {
+    {
+        'goolord/alpha-nvim',
+        dependencies = { 'echasnovski/mini.icons', 'nvim-lua/plenary.nvim' },
+        config = function()
+            local dashboard = require("alpha.themes.dashboard")
+            dashboard.section.header.val = {
+                "",
+                "",
+                "",
+                "",
+                "",
+                "   ▗▖▗▄▄▄▖▗▖  ▗▖ ▗▄▖ ▗▖  ▗▖▗▄▄▄▗▄▄▄▖▗▄▖ ▗▖  ▗▖▗▄▄▄▖ ▗▄▄▖",
+                "   ▐▌  █  ▐▛▚▖▐▌▐▌ ▐▌▐▛▚▖▐▌▐▌  █ █ ▐▌ ▐▌▐▛▚▖▐▌  █  ▐▌",
+                "   ▐▌  █  ▐▌ ▝▜▌▐▛▀▜▌▐▌ ▝▜▌▐▌  █ █ ▐▌ ▐▌▐▌ ▝▜▌  █  ▐▌",
+                "▗▄▄▞▘▗▄█▄▖▐▌  ▐▌▐▌ ▐▌▐▌  ▐▌▐▙▄▄▀ █ ▝▚▄▞▘▐▌  ▐▌▗▄█▄▖▝▚▄▄▖",
+            }
+
+            require 'alpha'.setup(dashboard.config)
+        end
+    },
+}
