3ddda5aab4dc1a436fa614d530d4ebb2fc6c2133
周金 <steve.zhou@dbappsecurity.com.cn>
2025-02-14 21:01:25 -0800

    use rustaceamvim

--- lua/jinandtonic/init.lua
+++ lua/jinandtonic/init.lua
@@ -39,14 +39,23 @@ autocmd('LspAttach', {
     group = JinAndTonic,
     callback = function(e)
         local opts = { buffer = e.buf }
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
-        vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
-        vim.keymap.set("n", "go", function() vim.lsp.buf.type_definition() end, opts)
-        vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "gs", function() vim.lsp.buf.signature_help() end, opts)
-        vim.keymap.set("n", "gE", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end,
+            { buffer = e.buf, desc = "Go to definition" })
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end,
+            { buffer = e.buf, desc = "Show hover information" })
+        vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end,
+            { buffer = e.buf, desc = "Go to declaration" })
+        vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end,
+            { buffer = e.buf, desc = "Go to implementation" })
+        vim.keymap.set("n", "go", function() vim.lsp.buf.type_definition() end,
+            { buffer = e.buf, desc = "Go to type definition" })
+        vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end,
+            { buffer = e.buf, desc = "Show references" })
+        vim.keymap.set("n", "gs", function() vim.lsp.buf.signature_help() end,
+            { buffer = e.buf, desc = "Show signature help" })
+        vim.keymap.set("n", "gE", function() vim.diagnostic.open_float() end,
+            { buffer = e.buf, desc = "Show diagnostics in float window" })
+
         vim.api.nvim_create_autocmd("BufWritePre", {
             buffer = e.buf,
             callback = function()
