d31916322d60a5d60f33939dd28b430f3bebf632
jin <zoukba0816@gmail.com>
2025-10-24 15:00:38 +0800

    remove some annoying comment

--- lua/jinandtonic/lazy/alpha.lua
+++ lua/jinandtonic/lazy/alpha.lua
@@ -1,20 +1,3 @@
--- return {
---     {
---         'goolord/alpha-nvim',
---         dependencies = { 'echasnovski/mini.icons', 'nvim-lua/plenary.nvim' },
---         config = function()
---             local dashboard = require("alpha.themes.dashboard")
---             dashboard.section.header.val = {
---                 "   ▗▖▗▄▄▄▖▗▖  ▗▖ ▗▄▖ ▗▖  ▗▖▗▄▄▄▗▄▄▄▖▗▄▖ ▗▖  ▗▖▗▄▄▄▖ ▗▄▄▖",
---                 "   ▐▌  █  ▐▛▚▖▐▌▐▌ ▐▌▐▛▚▖▐▌▐▌  █ █ ▐▌ ▐▌▐▛▚▖▐▌  █  ▐▌",
---                 "   ▐▌  █  ▐▌ ▝▜▌▐▛▀▜▌▐▌ ▝▜▌▐▌  █ █ ▐▌ ▐▌▐▌ ▝▜▌  █  ▐▌",
---                 "▗▄▄▞▘▗▄█▄▖▐▌  ▐▌▐▌ ▐▌▐▌  ▐▌▐▙▄▄▀ █ ▝▚▄▞▘▐▌  ▐▌▗▄█▄▖▝▚▄▄▖",
---             }
---
---             require 'alpha'.setup(dashboard.config)
---         end
---     },
--- }
 return {
     {
         "goolord/alpha-nvim",
